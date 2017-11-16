fb06daff0f0ccc8ff7e898bc6e140cab2c0d2145
Jamie Woodmancy <jamie29w@gmail.com>
2017-11-16 09:42:18 -0700

    added sign in and sign up

--- main/Login/Component.js
+++ main/Login/Component.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    TouchableOpacity
+} from "react-native";
+// import { Button } from "react-native-elements";
+// import Button from "react-native-button";
 
 export default function LoginComponent(props) {
     const styles = StyleSheet.create({
@@ -17,12 +26,74 @@ export default function LoginComponent(props) {
             paddingLeft: 10,
             paddingRight: 10,
             marginTop: 10,
-            marginBottom: 10
+            marginBottom: 10,
+            borderRadius: 5
+        },
+        submitButton: {
+            height: 40,
+            width: 300,
+            padding: 10,
+            backgroundColor: "#4C94F6",
+            borderColor: "#4C94F6",
+            borderBottomLeftRadius: 5,
+            borderTopLeftRadius: 5,
+            borderBottomRightRadius: 5,
+            borderTopRightRadius: 5
+        },
+        signInButton: {
+            height: 40,
+            width: 150,
+            padding: 10,
+            backgroundColor: "#4C94F6",
+            borderColor: "#4C94F6",
+            borderBottomLeftRadius: 5,
+            borderTopLeftRadius: 5
+            // borderBottomRightRadius: 5,
+            // borderTopRightRadius: 5
+        },
+        signUpButton: {
+            height: 40,
+            width: 150,
+            padding: 10,
+            backgroundColor: "#FAFAFA",
+            borderColor: "#FAFAFA",
+            // borderBottomLeftRadius: 5,
+            // borderTopLeftRadius: 5,
+            borderBottomRightRadius: 5,
+            borderTopRightRadius: 5
         }
     });
 
     return (
         <View style={styles.container}>
+            <View
+                style={{
+                    display: "flex",
+                    flexWrap: "no-wrap",
+                    flexDirection: "row"
+                }}>
+                <TouchableOpacity>
+                    <View style={styles.signInButton}>
+                        <Text style={{ textAlign: "center", color: "#FAFAFA" }}>
+                            Sign In
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.signUpButton}>
+                        <Text style={{ textAlign: "center", color: "#4C94F6" }}>
+                            Sign Up
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            <TextInput
+                style={styles.input}
+                placeholder="fist name"
+                onChangeText={props.onChangefName}
+                value={props.inputs.fName}
+                name="username"
+            />
             <TextInput
                 style={styles.input}
                 placeholder="username"
@@ -37,6 +108,15 @@ export default function LoginComponent(props) {
                 value={props.inputs.password}
                 name="password"
             />
+            {/* <Button style={styles.button}>SUBMIT</Button> */}
+            {/* <Button title="SUBMIT" color="#4C94F6" /> */}
+            <TouchableOpacity>
+                <View style={styles.submitButton}>
+                    <Text style={{ textAlign: "center", color: "#FAFAFA" }}>
+                        Try this
+                    </Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
--- main/Login/Container.js
+++ main/Login/Container.js
@@ -7,12 +7,18 @@ export default class LoginContainer extends Component {
         super();
         this.state = {
             inputs: {
+                fName: "",
                 username: "",
                 password: ""
             }
         };
+        this.onChangefName = this.onChangefName.bind(this);
         this.onChangeUsername = this.onChangeUsername.bind(this);
         this.onChangePw = this.onChangePw.bind(this);
+    }
+
+    onChangefName(text) {
+        this.setState({ inputs: { username: text } });
     }
 
     onChangeUsername(text) {
@@ -26,6 +32,7 @@ export default class LoginContainer extends Component {
     render() {
         return (
             <LoginComponent
+                onChangefName={this.onChangefName}
                 onChangeUsername={this.onChangeUsername}
                 onChangePw={this.onChangePw}
                 inputs={this.state.inputs}

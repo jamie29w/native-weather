6056861913d96983ccd407f4044af634ae343d7e
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-11-20 11:41:56 -0700

    complete weather class with methods to update state with forecast objects

--- client/main/Forecast/Weather.js
+++ client/main/Forecast/Weather.js
@@ -15,31 +15,25 @@ class Weather extends React.Component {
         super();
         this.state = {
             current: {},
-            position: {}
+            daily: {},
+            hourly: {}
         }
     }
 
     getLocation() {
         navigator.geolocation.getCurrentPosition(position => {
-            const thisPosition = position;
-            this.setState(prevState => {
-                return {
-                    ...prevState,
-                    position: thisPosition
-
-                }
-            })
+            this.getWeather(position.coords.latitude, position.coords.longitude);
         });
-        this.getWeather();
     }
 
-    getWeather() {
-        axios.get(`https://api.darksky.net/forecast/68f41e08c2748e697411c2fae78bcf0c/37.8267,-122.4233`).then(response => {
+    getWeather(lat, long) {
+        axios.get(`https://api.darksky.net/forecast/68f41e08c2748e697411c2fae78bcf0c/${lat},${long}`).then(response => {
             this.setState(prevState => {
-                console.log("in get")
                 return {
                     ...prevState,
-                    current: response.data.currently
+                    current: response.data.currently,
+                    daily: response.data.daily,
+                    hourly: response.data.hourly
                 }
             });
         });
@@ -50,15 +44,9 @@ class Weather extends React.Component {
     }
 
     render() {
-        console.log(this.state.position);
         return (
             <View style={{display: "flex", flex: 1, justifyContent: "center", alignItems: "center"}}>
-                {/* <WeatherWidget
-                    api={"68f41e08c2748e697411c2fae78bcf0c"}
-                    lat={"51.5033640"}
-                    long={"-0.1276250"}
-                /> */}
-                <Text>something</Text>
+                <Text>{this.state.current.summary}</Text>
             </View>
         )
     }
